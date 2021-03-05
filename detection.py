09ffbf42686abf1ea527a480adb9902c5661d919
JaminCO <76646536+JaminCO@users.noreply.github.com>
2021-03-05 20:39:55 +0100

    Add files via upload

---
+++ detection.py
@@ -0,0 +1,69 @@
+# OpenCV program to detect face in real time 
+# import libraries of python OpenCV  
+# where its functionality resides 
+import cv2  
+  
+# load the required trained XML classifiers 
+# https://github.com/Itseez/opencv/blob/master/ 
+# data/haarcascades/haarcascade_frontalface_default.xml 
+# Trained XML classifiers describes some features of some 
+# object we want to detect a cascade function is trained 
+# from a lot of positive(faces) and negative(non-faces) 
+# images. 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') 
+  
+# https://github.com/Itseez/opencv/blob/master 
+# /data/haarcascades/haarcascade_eye.xml 
+# Trained XML file for detecting eyes 
+
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')  
+  
+# capture frames from a camera 
+cap = cv2.VideoCapture(0) 
+  
+width = int(cap.get(3))
+height = int(cap.get(4))
+
+# loop runs if capturing has been initialized. 
+while 1:  
+  
+    # reads frames from a camera 
+    ret, img = cap.read()  
+    
+
+    font = cv2.FONT_ITALIC
+
+    # convert to gray scale of each frames 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+  
+    # Detects faces of different sizes in the input image 
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4) 
+  
+    for (x,y,w,h) in faces: 
+        # To draw a rectangle in a face  
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)  
+        roi_gray = gray[y:y+h, x:x+w] 
+        roi_color = img[y:y+h, x:x+w] 
+  
+        # Detects eyes of different sizes in the input image 
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)  
+  
+        #To draw a rectangle in eyes 
+        for (ex,ey,ew,eh) in eyes: 
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2) 
+    
+    frame = cv2.putText(img, "HI Welcome To A Webcam", (10, height - 10),  font, 1, (255, 1, 0), 5, cv2.LINE_AA)
+    # Display an image in a window 
+    cv2.imshow('img',frame) 
+  
+    # Wait for Esc key to stop 
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: 
+        break
+  
+# Close the window 
+cap.release() 
+  
+# De-allocate any associated memory usage 
+cv2.destroyAllWindows()
